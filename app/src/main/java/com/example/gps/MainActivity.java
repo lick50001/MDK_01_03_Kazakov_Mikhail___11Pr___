ade727a6f8cc94556f10f9ab68ee6094d8e512c5
anekk <aneckalarionova@gmail.com>
2026-04-07 14:39:28 +0500

    Разработанные переменные. Казаков

--- app/src/main/java/com/example/gps/MainActivity.java
+++ app/src/main/java/com/example/gps/MainActivity.java
@@ -1,24 +1,45 @@
 package com.example.gps;
 
+import android.location.Location;
+import android.location.LocationManager;
 import android.os.Bundle;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import java.text.BreakIterator;
+
 public class MainActivity extends AppCompatActivity {
+
+    LocationManager _LocationManager;
+    int ACCESS_FINE_LOCATION;
+    int ACCESS_COARSE_LOCATION;
+    TextView result;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+        };
+    }
+
+    @Override
+    public void onLocationChanged(@NonNull Location location){
+    if (location == null) return;
+    else {
+        String message = "";
+        if (location.getProvider().equals(LocationManager.GPS_PROVIDER)){
+            message += "\nМестоположение определено с помощью GPS: долгота -" + location.getLatitude() + "широта -" + location.getLongitude();
+        }
+        if (location.getProvider().equals(LocationManager.NETWORK_PROVIDER)){
+            message += "\nМестоположение определено с помощью интернета: долгота -" + location.getLatitude() + "широта -" + location.getLongitude();
+        }
+        result.setText(message);
     }
 }
